5a52ae9ab9b2747a8613124e664b5e0947b185b9
barrelblaze <shibi123online@gmail.com>
2025-09-17 12:02:51 +0530

    done

---
+++ biggest.py
@@ -0,0 +1,6 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+print("the biggest number is:",max(a,b,c))
+~                                                                                                                                                             
+~                                          
