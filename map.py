823f7b875e66528a8d27a82b46facb55729b0538
Rumaisa-321 <rumaisarumi809@gmail.com>
2025-11-06 10:08:35 +0530

    done

---
+++ map.py
@@ -0,0 +1,5 @@
+n = int(input("enter number of terms:"))
+powers = list(map(lambda x : 2**x,range(n)))
+print("powers of 2:",powers)
+~                                                                                                                                                             
+~  
