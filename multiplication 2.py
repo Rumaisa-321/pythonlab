1630f5d67993c982f19b3917ae2c1a6a20908e34
Rumaisa-321 <rumaisarumi809@gmail.com>
2025-11-06 10:10:37 +0530

    done

---
+++ multiplication 2.py
@@ -0,0 +1,8 @@
+n = int(input("enter a number:"))
+print("Multiplication table of",n)
+for i in range(1 , 11):
+        print(n, "x", i, "=", n * i)
+
+~                                                                             
+~                                                                             
+~                  
